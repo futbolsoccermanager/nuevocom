34a2d1704f9fbfdf945bbae834c6aa419dec0004
Ruben Hernando <hernando.ruben@gmail.com>
2012-10-04 14:59:58 +0200

    inicio. bootstrap. devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuevocom::Application.config.secret_token = '51c3c54b46c1c1a36e291acc1061c99a60ced9e514a410621e4d91de323f3ef0b7edf8413e3e0fa302a3b280aa3c6b5ff57258f0d5dd5a2acda9dbbb89147dee'
