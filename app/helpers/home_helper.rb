7816ec989d12f63b7ea6c98ad86242bf89a4934b
Pablo <pablo@pablo-laptop.(none)>
2013-01-22 20:16:47 +0100

    dashboard alineacion y botones para seleccionar y cambiar de equipo

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -9,7 +9,7 @@ module HomeHelper
   def dashboard_action(accion)
     case accion
       when 'alineacion'
-        '#'
+        plantilla_selecciones_path(:id_seleccion => current_user.current_seleccion(session).id)
       when 'fichajes'
         mercado_path(current_user.current_seleccion(session))
       when 'miliga'
