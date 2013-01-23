c96ea13021568b487fff581757db126c819b0d70
Ruben Hernando <rhernando@informa.es>
2013-01-23 19:22:13 +0100

    tabla clasificacion
    clasificacion

--- app/controllers/clasificacion_controller.rb
+++ app/controllers/clasificacion_controller.rb
@@ -1,2 +1,23 @@
 class ClasificacionController < ApplicationController
+  def index
+    liga = current_user.current_seleccion(session).liga
+
+    @equipos_liga = liga.selecciones
+
+    @puntos_liga = {}
+    @equipos_liga.map{|x| x.id}.each do |idsele|
+      hequipo = {}
+
+      puntos = OnceJornada.all(:seleccion_id => idsele)
+      total = 0.0
+      puntos.each do |pj|
+        total += pj.puntuacion
+        hequipo["jornada_#{pj.jornada}".to_sym] = pj.puntuacion
+      end
+      hequipo[:total] = total
+      @puntos_liga[idsele] = hequipo
+    end
+
+
+  end
 end
--- app/helpers/clasificacion_helper.rb
+++ app/helpers/clasificacion_helper.rb
@@ -1,2 +1,11 @@
 module ClasificacionHelper
+
+  def puntos_jornada_seleccion(puntuaciones, jornada, seleccion)
+    puntuaciones[seleccion]["jornada_#{jornada}".to_sym]
+  end
+
+  def puntos_total_seleccion(puntuaciones, seleccion)
+    puntuaciones[seleccion][:total]
+  end
+
 end
